225de1b449890ab336e3a46f8ff9bc626b0bdc99
Ascenio <ascenio@hotmail.com>
2021-09-11 18:54:38 -0300

    fix: import of menu

--- FastFingers/FastFingers.cpp
+++ FastFingers/FastFingers.cpp
@@ -1,6 +1,6 @@
 #include "Engine.h"
 #include "Resources.h"
-#include "Home.h"
+#include "Menu.h"
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 	Engine* engine = new Engine();
