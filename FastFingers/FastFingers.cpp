537637f481a97c640c5bfe91f7df3d880b85822e
Ascenio <ascenio@hotmail.com>
2021-09-11 17:37:03 -0300

    chore: remove leftovers

--- FastFingers/FastFingers.cpp
+++ FastFingers/FastFingers.cpp
@@ -1,42 +1,16 @@
-/**********************************************************************************
-// PacMan (C�digo Fonte)
-//
-// Cria��o:     01 Jan 2013
-// Atualiza��o: 25 Ago 2021
-// Compilador:  Visual C++ 2019
-//
-// Descri��o:   Exerc�cio sobre detec��o de colis�o
-//
-**********************************************************************************/
-
 #include "Engine.h"
 #include "Resources.h"
 #include "Home.h"
 
-// ------------------------------------------------------------------------------
-//                                  WinMain                                      
-// ------------------------------------------------------------------------------
-
-int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
-{
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 	Engine* engine = new Engine();
-
-	// configura a janela do jogo
 	engine->window->Mode(WINDOWED);
 	engine->window->Size(960, 620);
 	engine->window->Color(255, 255, 255);
 	engine->window->Title("FastFingers");
 	engine->window->Icon(IDI_ICON);
-
-	// configura dispositivo gr�fico
-	//engine->graphics->VSync(true);
-
-	// inicia o jogo
 	int status = engine->Start(new Menu());
 
 	delete engine;
 	return status;
 }
-
-// ----------------------------------------------------------------------------
-
